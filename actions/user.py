144b151ed47292a4b34c2c6e8ad65d87502546c1
Jorge1 <agullojorge@gmail.com>
2023-10-31 18:13:03 +0100

    Added nextActions

--- actions/user.py
+++ actions/user.py
@@ -30,4 +30,40 @@ class Actions:
         if len(login) == 0:
             print(strings.ANSWER_LOGIN_FAIL)
         else:
-            print(strings.ANSWER_LOGIN_SUCCESS % (login[1]))
+            username = login[1]
+            print(strings.ANSWER_LOGIN_SUCCESS % (username))
+            self._next_actions(username)
+            
+    def _next_actions(self, username):
+        print(strings.QUESTION_ACCTIONS_TODO)
+        
+        action = input(strings.QUESTION_WANT_TODO % (username))
+        
+        action_list = [
+            strings.CREATE,
+            strings.NOTE,
+            strings.DELETE,
+            strings.EXIT,
+        ]
+        
+        if action == action_list[0]:
+            print(strings.GO_TO % (action_list[0]))
+            self._next_actions(username)
+            
+        if action == action_list[1]:
+            print(strings.GO_TO % (action_list[1]))
+            self._next_actions(username)
+            
+        if action == action_list[2]:
+            print(strings.GO_TO % (action_list[2]))
+            self._next_actions(username)
+            
+        if action == action_list[3]:
+            print(strings.EXIT_TEXT % (username))
+            exit()
+            
+        if action not in action_list:
+            print('Elige una acción correcta')
+            self._next_actions(username)
+            
+        
