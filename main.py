d74cb0efb9532894a446b1c3b47d08c0de7ba4fc
Jorge1 <agullojorge@gmail.com>
2023-10-31 17:35:16 +0100

    (WIP) implement reg/login

--- main.py
+++ main.py
@@ -7,3 +7,21 @@ Proyecto Python y MySql:
 - Crear, mostrar y borrar notas
 """
 
+from constants import strings
+from actions.user import Actions as actions
+
+make = actions()
+
+print(f"""
+    Acciones disponibles:
+    - {strings.REGISTRO}
+    - {strings.LOGIN}      
+""")
+
+accion = input("¿Que quieres hacer?")
+
+if accion == strings.REGISTRO:
+    make.register()
+        
+if accion == strings.LOGIN:
+    make.login()
