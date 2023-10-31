1738f7f613ffc51497168df5b81f5e2ec7d08f2c
Jorge1 <agullojorge@gmail.com>
2023-10-31 18:12:52 +0100

    Added new strings

--- constants/strings.py
+++ constants/strings.py
@@ -3,16 +3,32 @@
 REGISTRO: str = 'registro'
 LOGIN: str = 'login'
 TO_DO: str = '¿Que quieres hacer? '
+CREATE: str = 'crear'
+NOTE: str = 'nota'
+DELETE: str = 'eliminar'
+EXIT: str = 'salir'
+GO_TO: str = 'Vamos a %s'
+EXIT_TEXT: str = 'Hasta luego %s!'
 
 ANSWER_REG: str = '\nOK!! Vamos a registrarte en el sistema...'
 ANSWER_REG_SUCCESS: str = '!Perfecto %s te has registrado con el email %s!'
 ANSWER_REG_FAIL: str = 'No se ha completado el registro correctamente.'
 
 ANSWER_LOGIN: str = 'Vale!! Identificate por favor...'
-ANSWER_LOGIN_SUCCESS: str = '¡Bienvenido %s!'
+ANSWER_LOGIN_SUCCESS: str = '¡Bienvenido %s! estás logeado.'
 ANSWER_LOGIN_FAIL: str = 'Error en usuario o contraseña'
 
 QUESTION_USERNAME: str = '¿Cual es tu nombre?: '
 QUESTION_SURNAME: str = '¿Cuales son tus apellidos?: '
 QUESTION_EMAIL: str = 'Introduce tu e-mail: '
 QUESTION_PASS: str = 'Introduce tu contraseña: '
+
+QUESTION_WANT_TODO: str = '¿Que quieres hacer %s?'
+QUESTION_ACCTIONS_TODO: str = f"""
+    Acciones disponibles:
+    - Crear nota ({CREATE})
+    - Mostrar notas ({NOTE})
+    - Eliminar nota ({DELETE})
+    - Salir ({EXIT})
+"""
+
