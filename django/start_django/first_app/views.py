6e2c9a783bcb42433b13dcf10e9bfbb2416f121b
Jorge1 <agullojorge@gmail.com>
2023-11-09 19:07:08 +0100

    hello world

--- django/start_django/first_app/views.py
+++ django/start_django/first_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+# MVC = Model View Controller
+# MVT = Model View Template (For Django)
+# In Django the View is the Template and the Controller is the View
+
+def hello_world(request):
+    return HttpResponse("""
+        <h1>Hello World Form Django<h3>
+        <h3> I am Jorge Agulló<h3>                    
+    """)
--- django/start_django/start_django/urls.py
+++ django/start_django/start_django/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# Import my views
+from first_app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('helloworld/', views.hello_world, name='helow_world')
 ]
