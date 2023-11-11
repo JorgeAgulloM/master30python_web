55229dcbc65fe37ea946141ccbc34b69ca0b7b85
Jorge1 <agullojorge@gmail.com>
2023-11-11 19:11:19 +0100

    Class forms update

--- django/start_django/first_app/forms.py
+++ django/start_django/first_app/forms.py
@@ -3,12 +3,28 @@ from django import forms
 class ArticleForm(forms.Form):
     title=forms.CharField(
         label='Title',
-        max_length=100
+        max_length=50,
+        min_length=5,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Write your title',
+                'class': 'title_form_article'
+            }
+        )
     )
     content=forms.CharField(
         label='Content',
-        widget=forms.Textarea
+        widget=forms.Textarea,
+        max_length=2500,
+        min_length=25
     )
+    content.widget.attrs.update( #No me gusta, pero tb funciona
+        {
+            'placeholder': 'Write your article',
+            'class': 'content_form_article'
+        } 
+    )
+    
     public=forms.TypedChoiceField(
         label='Is public?',
         choices=[        
