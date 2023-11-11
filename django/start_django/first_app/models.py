880bdb9799f3306ebb98245f36da051ed1127140
Jorge1 <agullojorge@gmail.com>
2023-11-11 12:02:33 +0100

    Added models

--- django/start_django/first_app/models.py
+++ django/start_django/first_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Aricle(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField()
+    public=models.BooleanField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+class Category(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.CharField(max_length=250)
+    creted_at=models.DateField()
