88bc588ae81e157d8b96ce761d7bc6335bc7b3a6
Jorge1 <agullojorge@gmail.com>
2023-11-12 16:35:49 +0100

    Listed articles

--- django/MyBlogDjango/blog/views.py
+++ django/MyBlogDjango/blog/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-
+from blog.models import Category, Article
 # Create your views here.
 
 def articles(request):
     
+    articles = Article.objects.all()
     
     params = {
-        'title': 'Articles'
+        'title': 'Articles',
+        'articles': articles,
     }
     
     return render(request, 'articles/list.html', params)
