14afcbc5e204f3016f60b0e9f515fbb7a2607233
Jorge1 <agullojorge@gmail.com>
2023-11-12 09:34:53 +0100

    Started first views

--- django/MyBlogDjango/main_app/views.py
+++ django/MyBlogDjango/main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    
+    params = {
+        'title': 'Index'
+    }
+    
+    return render(request, 'main_app/index.html', params)
+
+def about(request):
+    
+    params = {
+        'title': 'About'
+    }
+    
+    return render(request, 'main_app/about.html', params)
