51907024bcf9b4affbcbf3c46d43a80dc1ee4cf2
Jorge1 <agullojorge@gmail.com>
2023-11-12 20:29:58 +0100

    Added pagination

--- django/MyBlogDjango/blog/views.py
+++ django/MyBlogDjango/blog/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from blog.models import Category, Article
 # Create your views here.
 
 def articles(request):
     
+    # Obtener articulos
     articles = Article.objects.all()
+    
+    # Paginar articulos
+    paginator = Paginator(articles, 2)
+    
+    # Obtener número de página
+    page = request.GET.get('page')
+    page_articles = paginator.get_page(page)
     
     params = {
         'title': 'Articles',
-        'articles': articles,
+        'articles': page_articles,
     }
     
     return render(request, 'articles/list.html', params)
