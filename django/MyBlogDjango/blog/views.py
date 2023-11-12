8117acd7d16cbd52f0fd75deb170b701ff08fb72
Jorge1 <agullojorge@gmail.com>
2023-11-12 16:10:19 +0100

    Added new view and configured urls

--- django/MyBlogDjango/blog/views.py
+++ django/MyBlogDjango/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def articles(request):
+    
+    
+    params = {
+        'title': 'Articles'
+    }
+    
+    return render(request, 'articles/list.html', params)
