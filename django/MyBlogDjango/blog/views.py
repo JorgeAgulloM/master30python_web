b678a8d30a2ecba4b0aed16e87e3a59fcc6f73ab
Jorge1 <agullojorge@gmail.com>
2023-11-12 19:44:07 +0100

    Added article detaisl page

--- django/MyBlogDjango/blog/views.py
+++ django/MyBlogDjango/blog/views.py
@@ -13,6 +13,16 @@ def articles(request):
     
     return render(request, 'articles/list.html', params)
 
+def article(request, article_id):
+    
+    article = get_object_or_404(Article, id=article_id)
+    
+    params = {
+        'article': article,
+    }
+    
+    return render(request, 'articles/detail.html', params)
+
 def category(request, category_id):
     
     category = get_object_or_404(Category, id=category_id)
