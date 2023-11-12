f1e92bceebe14a9c2899085c6933e32d985576fd
Jorge1 <agullojorge@gmail.com>
2023-11-12 10:00:28 +0100

    The model is transferred

--- django/MyBlogDjango/main_app/models.py
+++ django/MyBlogDjango/main_app/models.py
@@ -1,18 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Page(models.Model):
-    title=models.CharField(max_length=50, verbose_name='Page title')
-    content=models.TextField(verbose_name='Page content')
-    slug=models.CharField(unique=True, max_length=50, verbose_name='URL Friendly')
-    visible=models.BooleanField(verbose_name='Is visible?')
-    created_at=models.DateTimeField(auto_now_add=True, verbose_name='Created at')
-    updated_at=models.DateTimeField(auto_now=True, verbose_name='Updated at')
-
-    class Meta:
-        verbose_name='Page'
-        verbose_name_plural='Pages'
-        
-        def __str__(self) -> str:
-            return self.title
