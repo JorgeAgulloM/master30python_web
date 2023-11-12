0be698ed4395ae355ceb2966a7d2113dafad5efe
Jorge1 <agullojorge@gmail.com>
2023-11-12 19:30:52 +0100

    develop Categories context menu

--- django/MyBlogDjango/blog/views.py
+++ django/MyBlogDjango/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Category, Article
 # Create your views here.
 
@@ -12,3 +12,14 @@ def articles(request):
     }
     
     return render(request, 'articles/list.html', params)
+
+def category(request, category_id):
+    
+    category = get_object_or_404(Category, id=category_id)
+    
+    params = {
+        'title': 'Categories',
+        'category': category
+    }
+    
+    return render(request, 'categories/category.html', params)
