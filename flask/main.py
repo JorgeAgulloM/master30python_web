14e10680a0ce349465008b98db11dd7d33a115b6
Jorge1 <agullojorge@gmail.com>
2023-11-13 18:15:49 +0100

    Added styles, styled menú and date with context_processor

--- flask/main.py
+++ flask/main.py
@@ -1,6 +1,11 @@
 from flask import Flask, redirect, url_for, render_template
+from datetime import datetime
 
 app = Flask(__name__)
+
+@app.context_processor
+def date_now():
+    return {'now': datetime.now()}
 
 @app.route('/')
 def index():
