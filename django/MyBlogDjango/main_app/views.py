2bd9ff30121934f42ba87a5072282abf21d2bd2e
Jorge1 <agullojorge@gmail.com>
2023-11-13 10:42:19 +0100

    Added new user register basic form

--- django/MyBlogDjango/main_app/views.py
+++ django/MyBlogDjango/main_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -20,8 +21,19 @@ def about(request):
 
 def register_page(request):
     
+    register_form = UserCreationForm()
+    
+    if request.method=='POST':
+        register_form = UserCreationForm(request.POST)
+        
+        if register_form.is_valid():
+            register_form.save()
+            
+            return redirect('index')
+    
     params = {
-        'title': 'Register'
+        'title': 'Register',
+        'register_form': register_form
     }
     
     return render(request, 'users/register.html', params)
