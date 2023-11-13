2abffc178f94e69ca2ad879b9421c5c0492174da
Jorge1 <agullojorge@gmail.com>
2023-11-13 13:27:38 +0100

    Added basic routes

--- flask/main.py
+++ flask/main.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World with Flask | Jorge Agulló'
+    return '<h1> Hello World with Flask | Jorge Agulló </h1>'
+
+@app.route('/info')
+def info():
+    return '<h1> Info Page </h1>'
+
+@app.route('/contact')
+def contact():
+    return '<h1> Contact page with Jorge Agulló </h1>'
+
+@app.route('/programing_laguages')
+def languages():
+    return '<h1> Languagues Page </h1>'
 
 if __name__=='__main__':
     app.run(debug=True)
